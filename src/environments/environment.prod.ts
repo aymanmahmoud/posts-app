f120cc49488b390c1a9f4b4c83316810394f9c94
Ayman Abo-El-Abbas <ayman.abbass61@gmail.com>
2020-07-16 18:01:23 +0200

    update json server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseURL: "https://jsonplaceholder.typicode.com/",
-  baseURL:"https://posts-app-7f6c4.firebaseio.com/"
+  // baseURL:"https://posts-app-7f6c4.firebaseio.com/",
+  baseURL :'http://localhost:3000/'
 };
